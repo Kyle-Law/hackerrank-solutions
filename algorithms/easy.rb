8bc6f474884e1c64dfd58c2810fd812cf59e5617
kyle-law <junwei_1121@hotmail.com>
2020-03-07 00:44:08 +0800

    Solved Lisa's Workbook

--- algorithms/easy.rb
+++ algorithms/easy.rb
@@ -764,26 +764,30 @@ def flatlandSpaceStations(n, c)
     end
     arr.max
 end
-
 # Same time limit problem.
-
+# Key takeaway: 19/20 done. Try to refactor your code, think of a way without using 2 loops :)
+# Methods used: #min, #abs, #Array.new, #each_index
 
 # p Array.new(6)
 
 # p [2].all? {|i| i.even?}
 
-# def workbook_helper(limit, n) #problem per page, problems per chapter
-#     arr = Array.new(n/limit+1,Array.new())
-#     range = (1..n).to_a
-#     i = 0
-#     range.each{|i| arr}
-#     end
-#     arr
-# end
+def workbook(n, k, arr) #n = number of chapters, k = max probs per page, arr = arr of integers that denote the number of problems in each chapters (n == arr.size)
+    pages = [["notthing"]]
+    special = 0
+    arr.each do |i|
+        pages << Array.new(i) {|j| j+1} if i<=k
+        if i>k
+            ((i.to_f/k).ceil).times do |round|
+                round == (i.to_f/k).ceil - 1 && i%k != 0 ? pages << Array.new(i%k) {|j| j+1+round*k} : pages << Array.new(k) {|j| j+1+round*k}
+            end
+        end
+    end
+    pages.each_with_index{|v,i| special+=1 if v.include?(i)}
+    special
+end
 
-# multi_array[1] = 1
-# p multi_array
-# p Array.new(5,Array.new(4,0)) #=> [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-# p Array.new(5,1)
-# p workbook_helper(3,10) #=>[[1,2,3],[4,5,6],[7,8,9],[10]]
-# p Array.new(n/limit+1,Array.new())
+# p workbook(5,3,[4,2,6,1,10]) #=> 4
+
+# Key takeaway: Array.new(n,c) => building an array with n length, each takes value of c. Array.new(n) {|i| i} => |i| block variable refers to index (i.e. 0..n), eg. Array.new(5){|i| i} => [0,1,2,3,4]. #n.times {|i| puts i}, |i| in #times refers to index as well (i.e. 0..n).  I created a multi-array representing pages and their respective questions.
+# Methods used: #each_with_index, #include?, #Array.new, #ceil, #times
